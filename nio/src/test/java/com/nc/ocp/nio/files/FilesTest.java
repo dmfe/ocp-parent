0a6694491c62ed5fce650066d322b44210c4cc00
Dmitry Fedotov <dm.fe@yandex.ru>
2021-02-27 00:44:56 +0300

    Add nio test jar generation. Refactor nio tests.

--- nio/src/test/java/com/nc/ocp/nio/files/FilesTest.java
+++ nio/src/test/java/com/nc/ocp/nio/files/FilesTest.java
@@ -1,49 +1,81 @@
 package com.nc.ocp.nio.files;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import com.nc.ocp.nio.exceptions.OcpNioException;
+
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-import org.junit.platform.runner.JUnitPlatform;
-import org.junit.runner.RunWith;
 
 /**
  * FilesTest
  */
-@RunWith(JUnitPlatform.class)
-public class FilesTest {
+class FilesTest {
 
     private FilesSample fs = new FilesSample();
 
-    @DisplayName("Directory exists")
-    @Test
-    void directoryExistisTest() {
-        assertTrue(fs.checkFile("horse"));
+    @Nested
+    @DisplayName("Check files existense")
+    class CheckFilesExistence {
+        @DisplayName("Directory exists")
+        @Test
+        void directoryExistsTest() {
+            assertTrue(fs.checkFile("horse"));
+        }
+
+        @DisplayName("File exists")
+        @Test
+        void fileExistsTest() {
+            assertTrue(fs.checkFile("horse/food.txt"));
+        }
+
+        @DisplayName("File doesn't exist")
+        @Test
+        void fileDoesntExistsTest() {
+            assertFalse(fs.checkFile("horse/somefood.txt"));
+        }
     }
 
-    @DisplayName("File exists")
-    @Test
-    void fileExistisTest() {
-        assertTrue(fs.checkFile("horse/food.txt"));
-    }
+    @Nested
+    @DisplayName("Compare two files/directories")
+    class CompareTwoEntities {
+        @DisplayName("Directories are the same")
+        @Test
+        void directoriesAreTheSameTest() {
+            assertTrue(fs.isSameFiles("test-data/horse", "test-data/steed"));
+        }
 
-    @DisplayName("File doesn't exist")
-    @Test
-    void fileDoesntExistisTest() {
-        assertFalse(fs.checkFile("horse/somefood.txt"));
-    }
+        @DisplayName("Directories are different")
+        @Test
+        void directoriesAreDifferentTest() {
+            assertFalse(fs.isSameFiles("test-data/horse", "test-data/zebra"));
+        }
 
-    @DisplayName("Directories are the same")
-    @Test
-    void DirectoriesAreTheSameTest() {
-        assertTrue(fs.isSameFiles("test-data/horse", "test-data/steed"));
-    }
+        @DisplayName("Files are the same")
+        @Test
+        void filesAreTheSameTest() {
+            assertTrue(fs.isSameFiles("test-data/horse/food.txt", "test-data/zebra/food.source"));
+        }
 
-    @DisplayName("Files are the same")
-    @Test
-    void FilesAreTheSameTest() {
-        assertTrue(fs.isSameFiles("test-data/horse/food.txt", "test-data/zebra/food.source"));
+        @DisplayName("Files are different")
+        @Test
+        void filesAreDifferent() {
+            assertFalse(fs.isSameFiles("test-data/horse/food.txt", "test-data/horse/stalefood.txt"));
+        }
+
+        @DisplayName("One of the files is not exist")
+        @Test
+        void fileIsNotExists() {
+            OcpNioException ex = assertThrows(OcpNioException.class, () -> {
+                fs.isSameFiles("test-data/horse/foot.txt", "test-data/horse/stalefood.txt");
+            });
+
+            assertEquals("Error while comparing files: test-data/horse/foot.txt", ex.getMessage());
+        }
     }
 }
 
